64e31c4a910e62902e030f8e0e3e6f9ac35c6166
tonymbarton <36773585+tonymbarton@users.noreply.github.com>
2019-08-21 16:25:45 -0400

    Add files via upload

--- dlp.py
+++ dlp.py
@@ -6,6 +6,10 @@ def cfor_ssn(tmp_line):
     f=re.findall(r'(\d{3}-\d{2}-\d{4})',tmp_line)
     return f
 
+def cfor_cc(tmp_line):
+    f=re.findall(r'(\d{4}-\d{4}-\d{4}-\d{4})',tmp_line)
+    return f
+
 folder = input("Folder: ")
 
 for filename in os.listdir(folder):
@@ -15,9 +19,11 @@ for filename in os.listdir(folder):
             doc = docx.Document(absolute)
             for line in doc.paragraphs:
                 ssn_list=cfor_ssn(line.text)
+                cc_list=cfor_cc(line.text)
 
-                if len(ssn_list) != 0:
+                if len(ssn_list) != 0 or len(cc_list) != 0:
                     print(absolute)
+                    print(cc_list)
                     print(ssn_list)
         except:
             print("Can't Read " + absolute)
@@ -28,10 +34,20 @@ for filename in os.listdir(folder):
             with open(absolute,"r") as fh:
                 for line in fh.readlines():
                     ssn_list = cfor_ssn(line)
+                    cc_list = cfor_cc(line)
 
                     if len(ssn_list) != 0:
+                            print("----Filename----")
                             print(filename)
+                            print("----Line Found In----")
+                            print(line)
+                            print("----Data Found----")
                             print(ssn_list)
+                            print("\n")
+                    elif len(cc_list) != 0:
+                            print(filename)
+                            print(cc_list)
+                            print("\n")
         except:
             print(filename + " Could not be read")
 
